cbc18abb82f8fcc512af945718178ed05e885c4f
charan-crafts <mangalacharancharan@gmail.com>
2025-11-11 18:49:19 +0530

    Completed the chat endpoints

--- backend/controllers/chat.controllers.js
+++ backend/controllers/chat.controllers.js
@@ -1,6 +1,7 @@
 
 const userModel = require("../models/user.model");
 const chatModel = require("../models/chat.model")
+const mongoose = require("mongoose")
 // search the user by name or email
 
 // search ="name" or "email"
@@ -51,10 +52,7 @@ const search = async (req, res) => {
     }
 
 }
-
-
 // send the individual message
-
 const accessChats = async (req, res) => {
 
     const { receiverId } = req.body;
@@ -79,13 +77,12 @@ const accessChats = async (req, res) => {
                 { users: { $elemMatch: { $eq: receiverId } } },
             ]
         }).populate({
-            path:"users",
-            select:"userName email profileUrl"
+            path: "users",
+            select: "userName email profileUrl"
         })
             .populate("recentMessage")
 
-
-        if (previousChat.length>0) {
+        if (previousChat.length > 0) {
 
             return res.status(200).json({
                 success: true,
@@ -116,7 +113,153 @@ const accessChats = async (req, res) => {
         })
     }
 }
+const getAllChats = async (req, res) => {
+
+    try {
+
+        const userId = req.userId;
+
+        const chats = await chatModel.find({
+            users: { $elemMatch: { $eq: userId } }
+        }).populate("users", "-password -refreshToken")
+            .populate("groupAdmin", "-password -refreshToken")
+            .populate("recentMessage")
+            .sort({ updatedAt: -1 })
+
+        return res.status(200).json({
+            success: true,
+            data: chats
+        })
+
+    } catch (error) {
+        console.log("error", error);
+        return res.status(500).json({
+            success: false,
+            error: error
+        })
+    }
+}
+
+const createGroupChat = async (req, res) => {
+
+    const { groupName, users } = req.body;
+
+
+    const createGroupChat = await chatModel.create({
+        isGroup: true, groupAdmin: req.userId, users: [...users, req.userId], chatName: groupName
+    })
+
+    await createGroupChat.save()
+
+    return res.status(200).json({
+        success: true,
+        data: createGroupChat._id,
+        message: "Group created"
+    })
+
+}
+
+const renameGroup = async (req, res) => {
+
+    const { groupName, groupId } = req.body;
+    try {
+
+
+        const isgroup = await chatModel.findOne({ _id: groupId })
+
+        console.log(isgroup);
+
+
+        if (!isgroup || !isgroup.isGroup) {
+            return res.status(400).json({
+                success: false,
+                message: "Group doesn't exists"
+            })
+        }
+
+        isgroup.chatName = groupName
+        await isgroup.save()
+
+        return res.status(200).json({
+            success: true,
+            message: "Updated"
+        })
+
+    } catch (error) {
+        console.log("Error ", error);
+        return res.status(500).json({
+            success: false,
+            error: error
+        })
+    }
+}
+
+
+const addMemberToGroup = async (req, res) => {
+
+    const { groupId, membersId } = req.body;
+
+
+    try {
+
+
+        if (!mongoose.Types.ObjectId.isValid(groupId) || !mongoose.Types.ObjectId.isValid(membersId)) {
+            return res.status(400).json({
+                success: false,
+                message: "Userid or group id is not valid"
+            })
+        }
+
+        const findGroup = await chatModel.findById({ _id: groupId })
+
+        console.log(findGroup);
+
+
+        if (!findGroup) {
+            return res.status(400).json({
+                success: false,
+                message: "Group not exists"
+            })
+        }
+
+
+        const findUser = await userModel.findById(membersId);
+
+        if (!findUser) {
+            return res.status(200).json({
+                success: true,
+                message: "User not found"
+            })
+        }
+
+        // add the users 
+
+        const updateUsers = await chatModel.findByIdAndUpdate(
+            groupId,
+             { $addToSet: { users: membersId } },
+            {new:true}
+        )
+
+        return res.status(200).json({
+            success:true,
+            message:"Added"
+        })
+    } catch (error) {
+        console.log("error ", error);
+        return res.status(200).json({
+            success: true,
+            message: "User not found"
+        })
+    }
+}
+
+// Group remove
+
 module.exports = {
     search,
-    accessChats
+    accessChats,
+    getAllChats,
+    createGroupChat,
+    renameGroup,
+    addMemberToGroup
 }
--- backend/routes/chat.routes.js
+++ backend/routes/chat.routes.js
@@ -12,4 +12,19 @@ router.get("/search",authMiddleware.authorization,chatController.search)
 // http://localhost:3000/api/v1/chat
 router.post("/",authMiddleware.authorization,chatController.accessChats)
 
+//http://localhost:3000/api/v1/chat
+//Get the all chats of the particular user
+router.get("/",authMiddleware.authorization,chatController.getAllChats)
+
+// Create a group chat
+
+router.post("/createGroup",authMiddleware.authorization,chatController.createGroupChat)
+
+
+// Rename the group
+
+router.post("/rename",authMiddleware.authorization,chatController.renameGroup)
+
+router.put("/addMember",authMiddleware.authorization,chatController.addMemberToGroup)
+
 module.exports = router
